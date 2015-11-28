60c7035c5c75dcf58b73b5a3d5c9bb8160a807d9
art <zinenkoartem@gmail.com>
2015-11-28 18:24:51 +0500

    Заглушки для первого уровня

---
+++ src/Controllers/CsrfController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CWS.Controllers
+{
+    public class CsrfController : Controller
+    {
+        public ActionResult Level1()
+        {
+            return View();
+        }
+    }
+}
