b02a6f7f05b9ec896c57ed581ed3ff689a19e483
art <zinenkoartem@gmail.com>
2015-11-29 16:44:03 +0500

    xss level 2

--- src/Controllers/XssController.cs
+++ src/Controllers/XssController.cs
@@ -10,10 +10,31 @@ namespace CWS.Controllers
     [ValidateInput(false)]
     public class XssController : Controller
     {
+        private static readonly List<Uri> links = new List<Uri>();
+
+        [HttpGet]
         public ActionResult Level1(string query)
         {
             Response.AddHeader("X-XSS-Protection", "0");
             return View((object)query);
+        }
+
+        [HttpGet]
+        public ActionResult Level2()
+        {
+            return View(links);
+        }
+
+        [HttpPost]
+        public ActionResult Level2(string link)
+        {
+            Uri uri;
+            if (Uri.TryCreate(link, UriKind.Absolute, out uri)) {
+                links.Add(uri);
+            } else {
+                ViewBag.UriParseErrorLink = link;
+            }
+            return View(links);
         }
     }
 }
---
+++ src/Scripts/checker.js
@@ -0,0 +1,25 @@
+﻿var levelCompleted = function() {
+    $('#task').hide();
+    $('#nextLevel').show();
+};
+
+var originalAlert = window.alert;
+var isAlerted = false;
+var isDocumentReady = false;
+
+window.alert = function(s) {
+    if (isDocumentReady) {
+        levelCompleted();
+    }
+    else {
+        isAlerted = true;
+    }
+    originalAlert(s);
+};
+
+$(function(){
+    if (isAlerted) {
+        levelCompleted();
+    }
+    isDocumentReady = true;
+});
