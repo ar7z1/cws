4576c4979730b31d78d2461dad34eea4c3443b71
art <zinenkoartem@gmail.com>
2015-11-29 15:48:21 +0500

    Первое задание на XSS

--- src/Controllers/XssController.cs
+++ src/Controllers/XssController.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Text.RegularExpressions;
 
 namespace CWS.Controllers
 {
+    [ValidateInput(false)]
     public class XssController : Controller
     {
-        public ActionResult Level1()
+        public ActionResult Level1(string query)
         {
-            return View();
+            Response.AddHeader("X-XSS-Protection", "0");
+            return View((object)query);
         }
     }
 }
