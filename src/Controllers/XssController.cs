b0f35e7cd1746ff43bd01fa94732d21098aa2e10
art <zinenkoartem@gmail.com>
2016-03-25 18:13:14 +0500

    Начал делать блок Защиты

--- src/Controllers/XssController.cs
+++ src/Controllers/XssController.cs
@@ -1,50 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
-namespace CWS.Controllers
+namespace CWS
 {
-    [ValidateInput(false)]
     public class XssController : Controller
     {
-        private static readonly List<Uri> links = new List<Uri>();
-
-        [HttpGet]
-        public ActionResult Level1(string query)
-        {
-            Response.AddHeader("X-XSS-Protection", "0");
-            return View((object) query);
-        }
-
-        [HttpGet]
-        public ActionResult Level2()
-        {
-            return View(links);
-        }
-
-        [HttpPost]
-        public ActionResult Level2(string link)
-        {
-            Uri uri;
-            if (Uri.TryCreate(link, UriKind.Absolute, out uri))
-            {
-                links.Add(uri);
-            }
-            else
-            {
-                ViewBag.UriParseErrorLink = link;
-            }
-            return View(links);
-        }
-
-        [HttpGet]
-        public ActionResult Level3()
-        {
-            return View();
-        }
-
-        [HttpGet]
-        public ActionResult Level4()
+        public ViewResult Index()
         {
             return View();
         }
